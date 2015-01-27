f844b7f8ca196bf9f9169b9a46a0ae20cb22c8c8
Dan Olsen <dan@straightbit.com>
2015-01-26 18:22:08 -0700

    Created a profile page. Redirect to login screen after registering. Started the products page.

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template, request, redirect
 from flask.ext.bootstrap import Bootstrap
 import requests
 
+from utils import datetimeformat, stringtodate
+
 app = Flask(__name__)
 Bootstrap(app)
-app.config['API_ENDPOINT'] = 'http://catchmycommission.com/api/v1/'
+app.config['API_ENDPOINT'] = 'http://127.0.0.1:8111/api/v1/'
 
 ADMINS = ['dan@straightbit.com']
 import logging
@@ -15,6 +17,9 @@ mail_handler = SMTPHandler('127.0.0.1',
                            ADMINS, 'Your Application Failed')
 mail_handler.setLevel(logging.ERROR)
 app.logger.addHandler(mail_handler)
+
+app.jinja_env.filters['datetimeformat'] = datetimeformat
+app.jinja_env.filters['stringtodate'] = stringtodate
 
 @app.route('/')
 def landing():
@@ -27,13 +32,14 @@ def login():
     if token:
         return redirect('/home/')
     next = request.args.get('next', '/home/')
+    registered = request.args.get('registered', None)
     unauth = request.args.get('unauth', None)
 
     redirected = False
     if unauth:
         redirected = True
 
-    return render_template('login.html', next=next, redirected=redirected)
+    return render_template('login.html', next=next, redirected=redirected, registered=registered)
 
 
 @app.route('/logout/')
@@ -50,16 +56,27 @@ def home():
 def register():
     return render_template('register.html')
 
+@app.route('/profile/')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/products/')
+def products():
+    return render_template('products.html')
+
 
 @app.context_processor
 def inject_user():
     context = {'loggedin': False}
 
     token = request.cookies.get('token', None)
-    if token:
+    if token and request.path != '/login/':
         response = requests.get('%s%s' % (app.config['API_ENDPOINT'], 'users'),
                                 headers={'Authorization': 'Token %s' % token})
+        if response.status_code == 401:
+            context['redirect'] = '/login/?next=%s' % request.path
         response = json.loads(response.content)
+
         context['user'] = response
         context['loggedin'] = True
 
@@ -68,5 +85,5 @@ def inject_user():
 
 if __name__ == '__main__':
     # app.config['API_ENDPOINT'] = 'http://catchmycommission.com/api/v1/'
-    # app.config['API_ENDPOINT'] = 'http://127.0.0.1:8111/api/v1/'
-    app.run()
+
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+__author__ = 'danolsen'
+
+def datetimeformat(value, format='%Y-%m-%d %H:%M'):
+    return value.strftime(format)
+
+def stringtodate(value, format='%Y-%m-%dT%H:%M:%S.%fZ'):
+    return datetime.strptime(value, format)
+
