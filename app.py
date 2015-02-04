32586567a993996fcd3bf91a8575156b01503a8e
Dan Olsen <dan@straightbit.com>
2015-02-04 16:09:54 -0700

    Updates.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import json
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, abort, g, session, Response
+from flask.ext.babel import Babel
 from flask.ext.bootstrap import Bootstrap
 import requests
 
@@ -9,10 +13,14 @@ app = Flask(__name__)
 Bootstrap(app)
 app.config['API_ENDPOINT'] = 'http://catchmycommission.com/api/v1/'
 # app.config['API_ENDPOINT'] = 'http://127.0.0.1:8111/api/v1/'
+app.config['LANGUAGES'] = {'en': u'English', 'es': u'Español'}
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+babel = Babel(app)
 
 ADMINS = ['dan@straightbit.com']
 import logging
 from logging.handlers import SMTPHandler
+
 mail_handler = SMTPHandler('127.0.0.1',
                            'server-error@straightbit.com',
                            ADMINS, 'Your Application Failed')
@@ -22,9 +30,32 @@ app.logger.addHandler(mail_handler)
 app.jinja_env.filters['datetimeformat'] = datetimeformat
 app.jinja_env.filters['stringtodate'] = stringtodate
 
+
+@babel.localeselector
+def get_locale():
+    """Direct babel to use the language defined in the session."""
+    return session.get('current_lang', 'en')
+
+
+@app.before_request
+def before():
+    if request.view_args and 'lang_code' in request.view_args:
+        if request.view_args['lang_code'] not in ('es', 'en'):
+            return abort(404)
+        g.current_lang = request.view_args['lang_code']
+        session['current_lang'] = request.view_args['lang_code']
+        request.view_args.pop('lang_code')
+
+
 @app.route('/')
 def landing():
     return render_template('index.html')
+
+@app.route('/language/<lang_code>/')
+def language():
+    # session['lang'] = request.view_args['lang_code']
+    next = request.args.get('next', '/home/')
+    return redirect(next)
 
 
 @app.route('/login/')
@@ -57,9 +88,11 @@ def home():
 def register():
     return render_template('register.html')
 
+
 @app.route('/profile/')
 def profile():
     return render_template('profile.html')
+
 
 @app.route('/products/')
 def products():
